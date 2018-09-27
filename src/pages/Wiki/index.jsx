5e858ee575d7ff2b4d4dc554ee1ab19480600a00
chenxizhang <chenxi.zhang@covenantsql.io>
2018-09-27 21:37:15 +0800

    add quickstart loading

--- src/pages/Wiki/index.jsx
+++ src/pages/Wiki/index.jsx
@@ -2,11 +2,14 @@ import * as React from 'react'
 import ReactMarkdown from 'react-markdown'
 import { Link } from 'react-router-dom'
 
+import { Icon } from 'antd'
+
 import styles from './Wiki.css'
 
 class Wiki extends React.Component {
   state = {
-    raw: ''
+    raw: '',
+    fetching: true
   }
 
   componentDidMount () {
@@ -16,7 +19,11 @@ class Wiki extends React.Component {
         const parser = new DOMParser()
         const dataEl = parser.parseFromString(data, 'text/html')
         const raw = dataEl.querySelector('body').innerHTML
-        this.setState({ raw })
+        this.setState({ raw, fetching: false })
+      })
+      .catch(e => {
+        console.error(e)
+        this.setState({ raw: 'Empty', fetching: false })
       })
   }
 
@@ -26,6 +33,7 @@ class Wiki extends React.Component {
         <Link to='/'>
           Back to Faucet
         </Link>
+        {this.state.fetching && <div className={styles.loading}><Icon type="loading" /></div>}
         {
           this.state.raw &&
           <ReactMarkdown source={this.state.raw} />
